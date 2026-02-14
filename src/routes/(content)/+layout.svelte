3dbc1d4e1959373d2af71681bfb7fb9bcfff7843
kaptast <kaptas.tamas@gmail.com>
2026-02-14 20:53:56 +0100

    reorganize the routes

---
+++ src/routes/(content)/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	let { children } = $props()
+</script>
+
+<dap-ds-button ondds-click={() => alert('Button clicked!')}> Bejelentkezés </dap-ds-button>
+
+{@render children()}
+0 ada1231Il1
