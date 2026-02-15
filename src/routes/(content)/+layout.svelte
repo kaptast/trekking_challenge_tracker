681c85137e3c15c82cadbc325fc37c5ddb004535
kaptast <kaptas.tamas@gmail.com>
2026-02-15 19:12:53 +0100

    create a header component

--- src/routes/(content)/+layout.svelte
+++ src/routes/(content)/+layout.svelte
@@ -1,22 +1,9 @@
 <script lang="ts">
-	import { enhance } from '$app/forms'
+	import Header from '$lib/components/Header.svelte'
+
 	let { children, data } = $props()
 </script>
 
-<header>
-	{#if data.user}
-		<dap-ds-avatar src={data.user.image} alt="User Avatar" size="md"></dap-ds-avatar>
-
-		<form method="POST" action="/auth?/signOut" use:enhance>
-			<dap-ds-button htmlType="submit">Sign out</dap-ds-button>
-		</form>
-	{:else}
-		<form method="POST" action="/auth?/signInSocial" use:enhance>
-			<input type="hidden" name="provider" value="strava" />
-			<input type="hidden" name="callbackURL" value="/" />
-			<dap-ds-button htmlType="submit">Sign in with Strava</dap-ds-button>
-		</form>
-	{/if}
-</header>
+<Header user={data.user} />
 
 {@render children()}
