baac40a3632e286a6cc6ee0065a1c55a14e7e80d
kaptast <kaptas.tamas@gmail.com>
2026-02-24 23:02:16 +0100

    add icons

--- src/routes/(content)/+page.svelte
+++ src/routes/(content)/+page.svelte
@@ -7,7 +7,20 @@
 <div class="space-y-2 p-10">
 	<Card title="Active Challenge">
 		{#snippet icon()}
-			🥇
+			<svg
+				width="53"
+				height="30"
+				viewBox="0 0 53 30"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					d="M0 30L53 29L42 7L39 16L27 0L16 18.5L11.5 14L0 30ZM34 20.5L24 10.5L28 8L31 14.25L34 20.5Z"
+					fill="#6E7278"
+				/>
+			</svg>
 		{/snippet}
 
 		<h4 class="font-cursive text-xl">Spring Alpine Quest</h4>
@@ -34,7 +47,26 @@
 	<div class="flex gap-2">
 		<Card title="My Group">
 			{#snippet icon()}
-				🥇
+				<svg
+					width="30"
+					height="23"
+					viewBox="0 0 30 23"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M13.9999 7C13.9999 9.76142 11.7614 12 8.99994 12C6.23852 12 3.99994 9.76142 3.99994 7C3.99994 4.23857 6.23852 2 8.99994 2C11.7614 2 13.9999 4.23857 13.9999 7Z"
+						fill="#6E7278"
+					/>
+					<path
+						d="M26.9999 5C26.9999 7.76142 24.7614 10 21.9999 10C19.2385 10 16.9999 7.76142 16.9999 5C16.9999 2.23858 19.2385 0 21.9999 0C24.7614 0 26.9999 2.23858 26.9999 5Z"
+						fill="#6E7278"
+					/>
+					<path
+						d="M0 23H30V17L29 14L26 12L22 11L18 12L15.5 14L14.5 17H13.5L13 15.5L12 14L10.5 13H8.5L6.5 13.5L4.5 14L2 15.5L0.5 18.5L0 23Z"
+						fill="#6E7278"
+					/>
+				</svg>
 			{/snippet}
 
 			<h4 class="font-cursive text-xl">Mountain Goats</h4>
