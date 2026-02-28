c03a15f34dbcae2e723116c8b4cfb77d4072c898
kaptast <kaptas.tamas@gmail.com>
2026-02-28 23:03:25 +0100

    style the header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -13,22 +13,37 @@
 	let { user }: Props = $props()
 </script>
 
-<header class="chipped-corners bg-sand p-1">
+<header class="bg-sand p-1 pb-0">
 	<div class="chipped-corners bg-black p-0.5">
-		<div class="chipped-corners flex items-center justify-between bg-blue p-1">
-			<a href="/" class=""> LOGO </a>
+		<div class="chipped-corners bg-sand p-1">
+			<div class="chipped-corners bg-black p-0.5">
+				<div
+					class="chipped-corners flex items-center gap-x-4 bg-blue-dark p-1 font-bold text-sand-light"
+				>
+					<a href="/" class=""> LOGO </a>
 
-			<nav class="flex items-center gap-3 text-sm">
-				<a href="/activities"> Activities </a>
-				<a href="/teams"> Teams </a>
-			</nav>
-			{#if user}
-				<a href="/auth" class="size-10 rounded-full">
-					<img src={user?.image} alt={user.name} />
-				</a>
-			{:else}
-				<Button href="/auth" label="Sign in" />
-			{/if}
+					<nav class="flex items-center gap-3 text-lg uppercase">
+						<a href="/"> <span class="text-xl">[</span> Home <span class="text-xl">]</span></a>
+						<a href="/activities">
+							<span class="text-xl">[</span> Activities <span class="text-xl">]</span></a
+						>
+						<a href="/teams">
+							<span class="text-xl">[</span> Teams <span class="text-xl">]</span></a
+						>
+					</nav>
+
+					{#if user}
+						<a href="/auth" class="ml-auto flex items-center gap-x-1">
+							<span class="size-10 border-x-2 border-black">
+								<img src={user?.image} alt={user.name} />
+							</span>
+							{user.name}
+						</a>
+					{:else}
+						<Button href="/auth" label="Sign in" class="ml-auto" />
+					{/if}
+				</div>
+			</div>
 		</div>
 	</div>
 </header>
