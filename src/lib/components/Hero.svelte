624b33512852d07199a66d07a9dccc15dee91612
kaptast <kaptas.tamas@gmail.com>
2026-02-28 22:17:43 +0100

    create a hero component

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Button from './Button.svelte'
+</script>
+
+<div class="hero h-120"></div>
+
+<style>
+	.hero {
+		background-image: url('/hero.png');
+		background-size: cover;
+	}
+</style>
