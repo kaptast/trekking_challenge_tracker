fe7725b1d8cf666dbb11ec1a3bd521b3804ee0eb
kaptast <kaptas.tamas@gmail.com>
2026-02-28 22:17:58 +0100

    adjust the header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Wrapper from './Wrapper.svelte'
+	import Button from './Button.svelte'
 
 	type Props = {
 		user:
@@ -13,43 +13,22 @@
 	let { user }: Props = $props()
 </script>
 
-<Wrapper>
-	<header class="flex items-center justify-between px-3 pb-1">
-		<a href="/" aria-label="Home" class="flex items-center gap-2">
-			<svg class="size-10" viewBox="0 0 52 49">
-				<defs>
-					<clipPath id="avatar-clip">
-						<path
-							d="M26 0.5C40.1117 0.5 51.5 11.2727 51.5 24.5C51.5 37.7273 40.1117 48.5 26 48.5C11.8883 48.5 0.5 37.7273 0.5 24.5C0.5 11.2727 11.8883 0.5 26 0.5Z"
-						/>
-					</clipPath>
-				</defs>
-				<image
-					href={user?.image ??
-						'https://api.dicebear.com/9.x/avataaars/svg?backgroundColor=b6e3f4,c0aede,d1d4f9'}
-					x="0"
-					y="0"
-					width="52"
-					height="49"
-					preserveAspectRatio="xMidYMid meet"
-					clip-path="url(#avatar-clip)"
-				/>
-				<path
-					d="M26 0.5C40.1117 0.5 51.5 11.2727 51.5 24.5C51.5 37.7273 40.1117 48.5 26 48.5C11.8883 48.5 0.5 37.7273 0.5 24.5C0.5 11.2727 11.8883 0.5 26 0.5Z"
-					fill="none"
-					stroke="#6E7177"
-					stroke-width="2"
-				/>
-			</svg>
-		</a>
+<header class="chipped-corners bg-sand p-1">
+	<div class="chipped-corners bg-black p-0.5">
+		<div class="chipped-corners flex items-center justify-between bg-blue p-1">
+			<a href="/" class=""> LOGO </a>
 
-		<nav class="flex items-center gap-3 text-sm">
+			<nav class="flex items-center gap-3 text-sm">
+				<a href="/activities"> Activities </a>
+				<a href="/teams"> Teams </a>
+			</nav>
 			{#if user}
-				<span class="text-stone-600">{user.name}</span>
-				<a href="/auth" class="text-stone-700 underline hover:no-underline">Account</a>
+				<a href="/auth" class="size-10 rounded-full">
+					<img src={user?.image} alt={user.name} />
+				</a>
 			{:else}
-				<a href="/auth" class="text-stone-700 underline hover:no-underline">Sign in</a>
+				<Button href="/auth" label="Sign in" />
 			{/if}
-		</nav>
-	</header>
-</Wrapper>
+		</div>
+	</div>
+</header>
