fe21ff9a7ea1979f68c14be9235e716653780df0
kaptast <kaptas.tamas@gmail.com>
2026-03-03 23:09:58 +0100

    add a team image

--- src/routes/(content)/+page.svelte
+++ src/routes/(content)/+page.svelte
@@ -14,7 +14,7 @@
 		Túrakihívás 2026
 	</h1>
 
-	<Button label="Lépj be a kalandba!" href="/auth" />
+	<Button label="Lépj be a kalandba!" href="/auth" size="large" />
 </div>
 
 <div class="grid grid-cols-3 grid-rows-2 gap-4 pt-4">
@@ -32,7 +32,7 @@
 
 	<Card>
 		<div class="grid place-items-center p-4 text-center">
-			<div class="flex gap-2"></div>
+			<img src="/team.png" alt="Team" class="h-20" />
 
 			<h3 class="mt-4 text-2xl font-bold text-black uppercase">{m.createYourOwnTeam()}</h3>
 			<h4 class="text-base font-semibold text-black">{m.createOrJoinTeam()}</h4>
@@ -41,9 +41,7 @@
 
 	<Card>
 		<div class="grid place-items-center p-4 text-center">
-			<div class="flex gap-2">
-				<img src="/xp.png" alt="XP" class="size-20" />
-			</div>
+			<img src="/xp.png" alt="XP" class="size-20" />
 
 			<h3 class="mt-4 text-2xl font-bold text-black uppercase">{m.earnPoints()}</h3>
 			<h4 class="text-base font-semibold text-black">{m.earnPointsForTheLeaderboard()}</h4>
