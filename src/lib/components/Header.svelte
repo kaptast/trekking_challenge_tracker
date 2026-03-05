e5890e58045bfdbadeed3fe36491c62e4bfdfc48
kaptast <kaptas.tamas@gmail.com>
2026-03-05 23:53:50 +0100

    display the user info in a card

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/state'
-	import Button from './Button.svelte'
+	import UserInfo from './UserInfo.svelte'
 
 	type Props = {
 		user:
@@ -31,27 +31,14 @@
 
 <header class="border-y-2 border-black">
 	<div class="border-y-4 border-brown-400 bg-brown-600">
-		<div class="mx-auto flex max-w-7xl items-center gap-x-4 px-4 font-bold text-brown-100">
+		<div class="relative mx-auto flex max-w-7xl items-center gap-x-4 px-4 font-bold text-brown-100">
 			<nav class="flex items-center gap-3">
 				{@render link('/', 'Home')}
 				{@render link('/activities', 'Activities')}
 				{@render link('/teams', 'Teams')}
 			</nav>
 
-			{#if user}
-				<a href="/auth" class="ml-auto flex items-center gap-x-1">
-					<span class="grid size-10 place-items-center border-x-2 border-black bg-[#2E3440]">
-						<img
-							src={user?.image || '/avatar.png'}
-							alt={user.name}
-							class="size-11/12 object-cover"
-						/>
-					</span>
-					{user.name}
-				</a>
-			{:else}
-				<Button href="/auth" label="Sign in" class="ml-auto" />
-			{/if}
+			<UserInfo {user} />
 		</div>
 	</div>
 </header>
---
+++ src/lib/components/UserInfo.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { localizeHref } from '$lib/paraglide/runtime'
+	import Button from './Button.svelte'
+
+	type Props = {
+		user:
+			| {
+					name?: string | null | undefined
+					image?: string | null | undefined
+			  }
+			| undefined
+	}
+
+	let { user }: Props = $props()
+</script>
+
+<a href={localizeHref('/auth')} class="chipped-corners absolute right-4 bg-black p-0.5">
+	<div class="chipped-corners bg-brown-400 p-0.5">
+		<div class="chipped-corners bg-brown-600 px-4 py-1">
+			{#if user}
+				<span class="ml-auto flex items-center gap-x-1">
+					<img src={user?.image || '/avatar.png'} alt={user.name} class="size-10 object-cover" />
+				</span>
+			{:else}
+				<span>Sign in</span>
+			{/if}
+		</div>
+	</div>
+</a>
