863b132f86d4d236f47b5216b9719bc9e4117741
kaptast <kaptas.tamas@gmail.com>
2026-03-07 23:42:02 +0100

    style the profile avatar

--- src/lib/components/Auth/Profile.svelte
+++ src/lib/components/Auth/Profile.svelte
@@ -24,32 +24,83 @@
 	const stravaAccount = $derived(accounts.find((acc) => acc.providerId === 'strava'))
 
 	let seed = $state('KOMI')
+	let changedAvatar = $state(false)
 
 	const avatar = $derived(
 		createAvatar(pixelArt, {
 			seed,
-			size: 128
+			size: 80
 		}).toDataUri()
 	)
 
 	function randomizeAvatar() {
 		seed = Math.random().toString(36).substring(2, 10)
+		changedAvatar = true
+	}
+
+	function saveAvatar() {
+		changedAvatar = false
 	}
 </script>
 
-<Card>
-	<div class="space-y-4 p-2">
-		<p class="text-stone-600 text-sm">Signed in as <strong>{user.name}</strong></p>
-		<p class="text-stone-500 text-sm">{user.email}</p>
+<div class="profile-grid grid gap-4">
+	<Card class="account">
+		<div class="account-grid grid w-full gap-1 p-1">
+			<div class="avatar chipped-corners relative size-20 bg-brown-450">
+				<img src={avatar} alt="Avatar" class="size-20" />
 
-		<hr class="border-stone-200" />
+				<button
+					class="absolute inset-0 grid cursor-pointer place-items-center bg-black text-[#fff] opacity-0 transition-opacity hover:opacity-40"
+					onclick={randomizeAvatar}
+					title="Randomize Avatar"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						xmlns:xlink="http://www.w3.org/1999/xlink"
+						viewBox="0 0 24 24"
+						class="w-12"
+						><g
+							fill="none"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							><path d="M18 4l3 3l-3 3"></path><path d="M18 20l3-3l-3-3"></path><path
+								d="M3 7h3a5 5 0 0 1 5 5a5 5 0 0 0 5 5h5"
+							></path><path
+								d="M21 7h-5a4.978 4.978 0 0 0-2.998.998M9 16.001A4.984 4.984 0 0 1 6 17H3"
+							></path></g
+						></svg
+					>
+				</button>
 
-		<img src={avatar} alt="Avatar" class="h-24 w-24 rounded-full" />
-		<Button label="Randomize Avatar" onclick={randomizeAvatar} class="w-full" />
+				{#if changedAvatar}
+					<button
+						onclick={saveAvatar}
+						title="Save avatar"
+						class="chipped-corners absolute top-0 right-0 z-10 cursor-pointer bg-brown-200 p-0.5 text-brown-600 transition-colors hover:bg-brown-100"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							xmlns:xlink="http://www.w3.org/1999/xlink"
+							viewBox="0 0 24 24"
+							class="w-5"
+							><path
+								d="M17 3H3v18h18V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3s3 1.34 3 3s-1.34 3-3 3zm3-10H5V5h10v4z"
+								fill="currentColor"
+							></path></svg
+						>
+					</button>
+				{/if}
+			</div>
 
-		<hr class="border-stone-200" />
+			<div class="user-name text-2xl font-semibold uppercase">{user.name}</div>
+			<div class="email text-base font-medium">{user.email}</div>
 
-		<div class="space-y-2">
+			<div class="team chipped-corners bg-brown-200 p-1">Team</div>
+		</div>
+
+		<!-- <div class="space-y-2">
 			{#if stravaAccount}
 				{#if stravaAccount.providerId === 'strava'}
 					<p class="text-sm font-medium">Strava Connection</p>
@@ -74,6 +125,63 @@
 
 		<form method="POST" action="?/signOut" use:enhance>
 			<Button label="Sign out" type="submit" class="w-full" />
-		</form>
-	</div>
-</Card>
+		</form> -->
+	</Card>
+
+	<Card class="badges"></Card>
+	<Card class="stats"></Card>
+	<Card class="activities"></Card>
+	<Card class="last-activity"></Card>
+</div>
+
+<style>
+	.profile-grid {
+		grid-template-areas:
+			'account badges badges'
+			'stats badges badges'
+			'activities activities last-activity';
+	}
+
+	.profile-grid :global(.account) {
+		grid-area: account;
+	}
+
+	.account-grid {
+		grid-template-areas:
+			'avatar user-name'
+			'avatar email'
+			'team team';
+	}
+
+	.avatar {
+		grid-area: avatar;
+	}
+
+	.user-name {
+		grid-area: user-name;
+	}
+
+	.email {
+		grid-area: email;
+	}
+
+	.team {
+		grid-area: team;
+	}
+
+	.profile-grid :global(.badges) {
+		grid-area: badges;
+	}
+
+	.profile-grid :global(.stats) {
+		grid-area: stats;
+	}
+
+	.profile-grid :global(.activities) {
+		grid-area: activities;
+	}
+
+	.profile-grid :global(.last-activity) {
+		grid-area: last-activity;
+	}
+</style>
