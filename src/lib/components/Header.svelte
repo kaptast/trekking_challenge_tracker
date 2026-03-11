f2b7ad0f7822f90ee0aef9a1632a00e5e1371d0a
kaptast <kaptas.tamas@gmail.com>
2026-03-11 22:42:10 +0100

    make the header always the same size

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -21,10 +21,10 @@
 	<a
 		{href}
 		class={[
-			'chipped-corners text-3d px-2 py-0.5 font-pixel tracking-wider uppercase',
+			'chipped-corners text-3d border px-2 py-0.5 font-pixel tracking-wider uppercase',
 			active
-				? 'text-gold-500 border border-brown-900 bg-brown-800 text-shadow-gold-800'
-				: 'text-brown-100 text-shadow-black'
+				? ' border-brown-900 bg-brown-800 text-gold-500 text-shadow-gold-800'
+				: 'border-brown-600 text-brown-100 text-shadow-black'
 		].join(' ')}
 	>
 		{label}
@@ -33,7 +33,9 @@
 
 <header class="border-y-2 border-black">
 	<div class="border-y-4 border-brown-400 bg-brown-600">
-		<div class="relative mx-auto flex max-w-7xl items-center gap-x-4 px-4 font-bold text-brown-100">
+		<div
+			class="relative mx-auto flex max-w-7xl items-center gap-x-4 px-4 py-1 font-bold text-brown-100"
+		>
 			<nav class="flex items-center gap-3">
 				{@render link(localizeHref('/'), m.home())}
 				{@render link(localizeHref('/activities'), m.activities())}
--- src/lib/components/UserInfo.svelte
+++ src/lib/components/UserInfo.svelte
@@ -16,12 +16,14 @@
 
 <a href={localizeHref('/auth')} class="chipped-corners absolute right-4 bg-black p-0.5">
 	<div class="chipped-corners bg-brown-400 p-0.5">
-		<div class="chipped-corners bg-brown-600">
-			{#if user}
-				<img src={user?.image || '/avatar.png'} alt={user.name} class="size-10 object-cover" />
-			{:else}
-				<span>{m.login()}</span>
-			{/if}
-		</div>
+		{#if user}
+			<div class="chipped-corners bg-brown-600">
+				<img src={user?.image || '/avatar.png'} alt={user.name} class="size-12 object-cover" />
+			</div>
+		{:else}
+			<div class="chipped-corners bg-brown-600 px-1">
+				{m.login()}
+			</div>
+		{/if}
 	</div>
 </a>
