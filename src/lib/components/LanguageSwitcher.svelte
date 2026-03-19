13a6a65cb7b81b29ce22952eff840ade6dd0b6ea
kaptast <kaptas.tamas@gmail.com>
2026-03-19 23:28:19 +0100

    add icons for the language switcher

--- src/lib/components/LanguageSwitcher.svelte
+++ src/lib/components/LanguageSwitcher.svelte
@@ -2,6 +2,11 @@
 	import { getLocale, setLocale, locales } from '$lib/paraglide/runtime'
 
 	const currentLocale = $derived(getLocale())
+
+	const icons = {
+		hu: '🇭🇺',
+		en: '🇬🇧'
+	}
 </script>
 
 <div class="flex items-center gap-1">
@@ -9,13 +14,13 @@
 		<button
 			onclick={() => setLocale(locale)}
 			class={[
-				'chipped-corners cursor-pointer border px-2 py-0.5 font-pixel text-sm tracking-wider uppercase',
+				'chipped-corners grid cursor-pointer place-content-center border px-1.5 pt-2 pb-1 font-pixel text-lg leading-3 tracking-wider uppercase',
 				currentLocale === locale
 					? 'border-brown-900 bg-brown-800 text-gold-500 text-shadow-gold-800'
 					: 'hover:bg-brown-700 border-brown-600 text-brown-100 text-shadow-black'
 			]}
 		>
-			{locale}
+			{icons[locale]}
 		</button>
 	{/each}
 </div>
