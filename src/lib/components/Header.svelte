cd1b718c2a6e9c6747883cf9100d7ab855eb5877
kaptast <kaptas.tamas@gmail.com>
2026-03-19 22:43:50 +0100

    position the language switcher buttons

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -27,7 +27,7 @@
 			active
 				? ' border-brown-900 bg-brown-800 text-gold-500 text-shadow-gold-800'
 				: 'border-brown-600 text-brown-100 text-shadow-black'
-		].join(' ')}
+		]}
 	>
 		{label}
 	</a>
@@ -36,7 +36,7 @@
 <header class="border-y-2 border-black">
 	<div class="border-y-4 border-brown-400 bg-brown-600">
 		<div
-			class="relative mx-auto flex max-w-7xl items-center gap-x-4 px-4 py-1 font-bold text-brown-100"
+			class="relative mx-auto flex max-w-7xl items-center justify-between gap-x-4 px-4 py-1 font-bold text-brown-100"
 		>
 			<nav class="flex items-center gap-3">
 				{@render link(localizeHref('/'), m.home())}
@@ -44,23 +44,25 @@
 				{@render link(localizeHref('/teams'), m.teams())}
 			</nav>
 
-			<div class="ml-auto flex items-center gap-1">
-				{#each locales as locale}
-					<button
-						onclick={() => setLocale(locale)}
-						class={[
-							'chipped-corners cursor-pointer border px-2 py-0.5 font-pixel text-sm tracking-wider uppercase',
-							currentLocale === locale
-								? 'border-brown-900 bg-brown-800 text-gold-500 text-shadow-gold-800'
-								: 'hover:bg-brown-700 border-brown-600 text-brown-100 text-shadow-black'
-						].join(' ')}
-					>
-						{locale}
-					</button>
-				{/each}
-			</div>
+			<div class=" absolute right-4 flex items-center gap-4">
+				<div class="flex items-center gap-1">
+					{#each locales as locale (locale)}
+						<button
+							onclick={() => setLocale(locale)}
+							class={[
+								'chipped-corners cursor-pointer border px-2 py-0.5 font-pixel text-sm tracking-wider uppercase',
+								currentLocale === locale
+									? 'border-brown-900 bg-brown-800 text-gold-500 text-shadow-gold-800'
+									: 'hover:bg-brown-700 border-brown-600 text-brown-100 text-shadow-black'
+							]}
+						>
+							{locale}
+						</button>
+					{/each}
+				</div>
 
-			<UserInfo {user} />
+				<UserInfo {user} />
+			</div>
 		</div>
 	</div>
 </header>
