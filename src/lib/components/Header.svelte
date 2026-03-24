1fb7ee243db03e7d56cc7edb3c66cacc41542924
kaptast <kaptas.tamas@gmail.com>
2026-03-24 22:40:39 +0100

    set a max width for the header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -21,15 +21,15 @@
 <header class="fixed inset-x-0 top-0 z-50 w-screen border-y-2 border-brown-900">
 	<div class="border-b-4 border-brown-300 bg-brown-600">
 		<div
-			class="text-brown-100 flex items-center justify-between gap-x-4 px-2 py-0.5 font-bold lg:mx-auto lg:px-4"
+			class="text-brown-100 flex max-w-7xl items-center justify-between gap-x-4 px-2 py-0.5 font-bold sm:mx-auto sm:px-4"
 		>
-			<button class="flex w-5 cursor-pointer flex-col gap-y-1 lg:hidden" aria-label="Open menu">
+			<button class="flex w-5 cursor-pointer flex-col gap-y-1 sm:hidden" aria-label="Open menu">
 				<span class="h-0.5 w-full bg-sand"></span>
 				<span class="h-0.5 w-full bg-sand"></span>
 				<span class="h-0.5 w-full bg-sand"></span>
 			</button>
 
-			<nav class="hidden items-center lg:flex">
+			<nav class="hidden items-center sm:flex">
 				<Button
 					href={localizeHref('/')}
 					label={m.home()}
